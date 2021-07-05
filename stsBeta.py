4ebc5cbe119faf2e0eb711c3a4fc9259d3f90d6c
ilya <uriyvich@gmail.com>
2021-07-05 21:22:00 +0000

    checked news data against sp500

--- stsBeta.py
+++ stsBeta.py
@@ -2,6 +2,7 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = 'https://www.google.com/finance?tab=ne' # the site we are scrapeing
 res = requests.get(url)
@@ -24,6 +25,7 @@ blacklist = [
     'font',
     'html{}',
     'style',
+    'add_circle_outline'
     # there may be more elements you don't want, such as "style", etc.
 ]
 
@@ -31,7 +33,20 @@ for t in text: # filter out the data with our blacklist
     if t.parent.name not in blacklist:
         output += '{} '.format(t)
 
-# out put a clear clean and filterd data to our terminal
-print(output)
+# out put a clear clean and filterd data to our te
+out1 = (output.split())
+with open('snp500.csv') as f:
+ csvf = csv.reader(f)
+ ticker = []
+ name = []
+ for row in csvf:
+  ticker.append(row[0])
+  name.append(row[1])
+match = {}
+for word in out1:
+ result = word in ticker
+ if result == True:
+  match[word] = result
+print(match)
 
 
